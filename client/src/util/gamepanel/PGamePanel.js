731a32a7121f7cd03d9670640ef63a0e53be27c6
Timothy Ho <timothyhohyhtomit@gmail.com>
2025-12-22 14:27:02 -0700

    Put event handlers outside of rendering code to fix bug

--- client/src/util/gamepanel/PGamePanel.js
+++ client/src/util/gamepanel/PGamePanel.js
@@ -3,17 +3,21 @@ import React from "react";
 import "./PGamePanel.css";
 
 function PGamePanel({ status, setStatus }) {
+    // event handlers
+    const handleClickChangeTurn = (e) => {
+        setStatus(prevStatus => {
+            return {
+                ...prevStatus,
+                turn: -prevStatus.turn
+            };
+        });
+    };
     return (
         <div className="panel">
             <div className={`panel-turn ${status.turn === 1 ? "white" : "black"}`}>{status.turn === 1 ? "♔ White" : "♚ Black"}'s Turn</div>
             {/* FOR TESTING ONLY */}
             <div>TESTING</div>
-            {/*
-            <button onClick={setStatus(prevStatus => ({
-                ...prevStatus,
-                turn: -prevStatus.turn
-            }))}>Change Turn</button>
-            */}
+            <button onClick={handleClickChangeTurn}>Change Turn</button>
         </div>
     );
 }
